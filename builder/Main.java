3bd7993aa1a2873067e5ab1867ddd9b8ac4e3129
miskape <36408317+miskape@users.noreply.github.com>
2018-04-25 23:57:27 +0300

    Done

---
+++ builder/Main.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package builder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author miskape
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        BurgerBuilder mcdonalds = new McDonalds();
+        BurgerBuilder hesburger = new Hesburger();
+        Director director = new Director();
+        
+        director.setBurgerBuilder(hesburger);
+        director.constructBurger();
+        Burger hese = director.getHampurilainen();
+        List heseStructure = (ArrayList) hesburger.getBurger();
+        
+        System.out.println("Kanahampurilainen");
+        System.out.println(hese.toString());
+        System.out.println(heseStructure.toString());
+        
+        director.setBurgerBuilder(mcdonalds);
+        director.constructBurger();
+        Burger mäkkäri = director.getHampurilainen();
+        StringBuilder mäkkäriStructure = (StringBuilder) mcdonalds.getBurger();
+        
+        System.out.println("Big Mac");
+        System.out.println(mäkkäri.toString());
+        System.out.println(mäkkäriStructure.toString());
+        
+        
+    }
+}
